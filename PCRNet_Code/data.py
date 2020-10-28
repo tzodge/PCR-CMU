952eaeaaba7d2d21bf2cee7f6c89f92dcea9f7a5
vinits5 <vinitsarode5@gmail.com>
2020-10-28 18:44:23 +0530

    --no-check-certificate added in download function

--- PCRNet_Code/data.py
+++ PCRNet_Code/data.py
@@ -58,8 +58,9 @@ def download():
 	if not os.path.exists(DATA_DIR):
 		os.mkdir(DATA_DIR)
 	if not os.path.exists(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048')):
-		www = '--no-check-certificates https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
+		www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
 		zipfile = os.path.basename(www)
+		www += ' --no-check-certificate'
 		os.system('wget %s; unzip %s' % (www, zipfile))
 		os.system('mv %s %s' % (zipfile[:-4], DATA_DIR))
 		os.system('rm %s' % (zipfile))
