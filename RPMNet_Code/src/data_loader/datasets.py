e9daed2eca76fa3657bb375392825b52c7af1273
RahulChakwate <35038986+ruc98@users.noreply.github.com>
2020-10-28 10:02:38 +0530

    Added no-check-certificate to wget

--- RPMNet_Code/src/data_loader/datasets.py
+++ RPMNet_Code/src/data_loader/datasets.py
@@ -259,7 +259,7 @@ class ModelNetHdf(Dataset):
 
         www = 'https://shapenet.cs.stanford.edu/media/modelnet40_ply_hdf5_2048.zip'
         zipfile = os.path.basename(www)
-        os.system('wget {}'.format(www))
+        os.system('wget --no-check-certificate {}'.format(www))
         os.system('unzip {} -d .'.format(zipfile))
         os.system('mv {} {}'.format(zipfile[:-4], os.path.dirname(dataset_path)))
         os.system('rm {}'.format(zipfile))
